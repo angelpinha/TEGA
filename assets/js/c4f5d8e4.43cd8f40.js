12e9842efce3e418dfa088c1b28bf137b078ac0a
AngelPinha <services.angelpinha@gmail.com>
2021-06-25 18:03:17 -0400

    Deploy website - based on 25038f43c6de7111846c5103a6ced7574b09a25a

---
+++ assets/js/c4f5d8e4.43cd8f40.js
@@ -0,0 +1,1 @@
+(self.webpackChunktega=self.webpackChunktega||[]).push([[195],{9526:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c}});var r=n(7294),u=(n(9349),n(6742),n(2263),n(4996)),a=n(5977);var c=function(){return r.createElement(a.l_,{to:(0,u.Z)("/docs/prefacio")})}}}]);
